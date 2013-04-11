330b368b45f03f4d02442cd4200400a898a1c0b6
Frank Henard <frankhenard@fhenardmacbook2010.local>
2013-04-11 16:22:20 -0500

    first commit

---
+++ src/cli_reporting/core.clj
@@ -0,0 +1,6 @@
+(ns cli-reporting.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/cli_reporting/core_test.clj
@@ -0,0 +1,7 @@
+(ns cli-reporting.core-test
+  (:use clojure.test
+        cli-reporting.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
